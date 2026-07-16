f484e421f7f9fba619ee293c201c7bcfe343efb6
Enreap <devopsrepo@enreap.com>
2026-07-16 14:24:50 +0530

    removed file

--- src/main/java/org/springframework/samples/petclinic/DemoBug.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class DemoBug {
-
-    public static void main(String[] args) {
-        String s = null;
-        System.out.println(s.length()); // Possible NullPointerException
-    }
-}
